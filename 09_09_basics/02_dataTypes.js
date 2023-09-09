359c74defdc59993468cc6a4de30272248339eb8
Aman kumar <138039307+amankumar62030@users.noreply.github.com>
2023-09-09 19:56:33 +0000

    datatypes

---
+++ 09_09_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  //treat all JS code as newer version
+// alert(3+3)  //we r using node js not browser
+
+console.log(3 + 3)  //code readability should be high
+console.log("hi");
+
+
+let name="AMAN"
+let age=18
+let isLoggedIn=false
+let state;
+
+
+//numer => 2^53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//underfined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof "AMAN");
+console.log(typeof age);
+console.log(typeof undefined);
