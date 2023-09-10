f8879ed2c2c0ca1457068b97bbb8979d405832da
Aman kumar <138039307+amankumar62030@users.noreply.github.com>
2023-09-10 11:17:43 +0000

    dataTypessummary

---
+++ 09_09_basics/dataTypesSummary.js
@@ -0,0 +1,38 @@
+// Primitive DataType
+//7 categories: String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId);
+
+
+const bigNumber=27353534344352252n
+
+
+
+
+
+//Reference DataType (Non-primitive)
+
+//Array,Objects,Functions
+
+const heros=["shaktiman","doga","naagraj"]
+let myObj={
+    name:"aman",   //inside curly bracess are objects
+    age: 22,
+}
+
+const myFunction=function(){
+    console.log("hello world");
+}
+
+console.log(typeof outsideTemp);
+console.log(typeof myFunction);
+console.log(typeof anotherId);
